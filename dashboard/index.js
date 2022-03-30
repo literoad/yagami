de786e19cc31b9f8e4020edeb2a011bbc33591f7
Vyacheslav Pukhanov <vyacheslav@pukhanov.ru>
2022-03-30 20:28:08 +0300

    hide dashboard behind basic auth

--- dashboard/index.js
+++ dashboard/index.js
@@ -8,18 +8,36 @@ const { BullAdapter } = require("@bull-board/api/bullAdapter");
 const { FastifyAdapter } = require("@bull-board/fastify");
 
 fastify.register(require("fastify-graceful-shutdown"));
+fastify.register(require("fastify-basic-auth"), {
+  validate: async (login, password) => {
+    if (
+      login !== process.env.DASHBOARD_LOGIN ||
+      password !== process.env.DASHBOARD_PASSWORD
+    ) {
+      return new Error("Invalid authentication");
+    }
+  },
+  authenticate: true,
+});
 
 const queueAdapter = new BullAdapter(monitors);
 const serverAdapter = new FastifyAdapter();
+serverAdapter.setBasePath("/ui");
 
 createBullBoard({
   queues: [queueAdapter],
   serverAdapter,
 });
 
-fastify.register(serverAdapter.registerPlugin());
-
 fastify.after(() => {
+  fastify.register(serverAdapter.registerPlugin(), { prefix: "/ui" });
+
+  fastify.get("/", async (req, res) => {
+    res.redirect("/ui");
+  });
+
+  fastify.addHook("onRequest", fastify.basicAuth);
+
   fastify.gracefulShutdown((signal, next) => {
     next();
   });
