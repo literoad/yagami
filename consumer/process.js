b6c879a1bb349296ab6faba54aec96508c1622a6
Vyacheslav Pukhanov <vyacheslav@pukhanov.ru>
2022-03-30 21:41:12 +0300

    implement basic job consumer

--- consumer/process.js
+++ consumer/process.js
@@ -1,5 +1,14 @@
+const fetch = require("node-fetch");
+
 module.exports = async function (job) {
-  console.log(job);
-  console.log(job.data);
-  return true;
+  const reportResult = await fetch(process.env.RYUK_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      url: job.data.url,
+    }),
+  });
+  return await reportResult.json();
 };
